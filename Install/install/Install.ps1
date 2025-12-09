a20db909c23ebf8fdb14bf4fd9ae448ba73c2266
LoveKapibarasan <lovekapibarasan@lovekapibarasan.org>
2025-12-09 08:22:20 +0100

    commit

--- Install/install/Install.ps1
+++ Install/install/Install.ps1
@@ -54,3 +54,15 @@ Get-Disk
 Get-Partition -DriveLetter D
 Format-Volume -DriveLetter D -FileSystem NTFS -NewFileSystemLabel "Data" -Confirm:$false
 
+# Symbolik-Link
+## End Task Before
+Move-Item "$env:USERPROFILE\AppData\Local\Programs" "D:\Programs"
+Remove-Item "$env:USERPROFILE\AppData\Local\Programs"
+cmd /c mklink /D "$env:USERPROFILE\AppData\Local\Programs" "D:\Programs"
+cmd /c dir "$env:USERPROFILE\AppData\Local" | findstr Programs
+
+Move-Item "$env:USERPROFILE\AppData\Local\Python" "D:\Python"
+cmd /c mklink /D "$env:USERPROFILE\AppData\Local\Python" "D:\Python"
+
+Move-Item "$env:USERPROFILE\.ollama" "D:\.ollama"
+cmd /c mklink /D "$env:USERPROFILE\.ollama" "D:\.ollama"
