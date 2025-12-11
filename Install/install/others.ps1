34ef099fe36835a5eb90f8f7866cc97983038d0e
LoveKapibarasan <lovekapibarasan@lovekapibarasan.org>
2025-12-11 07:51:50 +0100

    commit

--- Install/install/others.ps1
+++ Install/install/others.ps1
@@ -7,3 +7,7 @@ choco install golang  -y
 
 # Image Magick
 choco install imagemagick ghostscript -y
+
+choco install Cygwin -y
+.\setup-x86_64.exe -q -P gcc-g++ -P wget -P tar -P gawk -P libiconv -P make -P curl
+setx PATH "$($env:PATH);D:\cygwin64\usr\bin"
