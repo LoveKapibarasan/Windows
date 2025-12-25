aa5e93c89fa0c10ffce82e390b24f2a4965b63d1
LoveKapibarasan <lovekapibarasan@lovekapibarasan.org>
2025-12-25 14:43:04 +0100

    commit

--- Install/install/disks.ps1
+++ Install/install/disks.ps1
@@ -18,3 +18,18 @@ cmd /c mklink /D "$env:USERPROFILE\.ollama" "D:\.ollama"
 
 Move-Item "C:\Python310" "D:\Python310"
 cmd /c mklink /D "C:\Python310" "D:\Python310"
+
+Move-Item "C:\Program Files\Chromium" "D:\Chromium"
+cmd /c mklink /D "C:\Program Files\Chromium" "D:\Chromium"
+
+Move-Item "C:\Program Files\Docker" "D:\Docker"
+cmd /c mklink /D "C:\Program Files\Docker" "D:\Docker"
+
+Move-Item "C:\Program Files\WSL" "D:\WSL"
+cmd /c mklink /D "C:\Program Files\WSL" "D:\WSL"
+
+wsl --list --verbose
+wsl --shutdown
+wsl --export docker-desktop D:\WSL_Docker\docker-desktop-data.tar
+wsl --unregister docker-desktop
+wsl --import docker-desktop D:\WSL_Docker\docker-desktop-data D:\WSL_Docker\docker-desktop-data.tar --version 2
